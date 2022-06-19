e1db026d9680bd642d3da059de0f7e45e6195a1c
flutterd3 <107663176+flutterd3@users.noreply.github.com>
2022-06-19 21:22:06 +0545

    htt.response res functin created sucessfully

--- lib/features/auth/services/auth_service.dart
+++ lib/features/auth/services/auth_service.dart
@@ -22,9 +22,16 @@ class AuthService {
           type: '',
           token: '');
 
-      http.post(
+      http.Response res = await http.post(
         Uri.parse("$uri/api.signup"),
+        body: user.toJson(),
+        headers: <String, String>{
+          'Content-type': 'application/json; charset=UTF-8',
+        },
       );
+
+      // catching error from our client side is not the issue but the issue is we have to catch it from our auth.js file in
+
     } catch (e) {}
   }
 }
