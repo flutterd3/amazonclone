e45831daaa4c85f9daee6740a93f2b7918d3d045
flutterd3 <107663176+flutterd3@users.noreply.github.com>
2022-06-21 22:11:14 +0545

    home screen setup for our routes

---
+++ lib/features/auth/home/homescreen.dart
@@ -0,0 +1,28 @@
+import 'package:amazonclone/providers/user_provider.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/src/foundation/key.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:provider/provider.dart';
+
+class HomeScreen extends StatefulWidget {
+  // to get home screen from authservice file we use this route
+  static const String routeName = '/home';
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    final user = Provider.of<UserProvider>(context).user;
+    return Scaffold(
+      body: Center(
+        child: Text(
+          user.toJson(),
+        ),
+      ),
+    );
+  }
+}
